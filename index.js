6a85923bda6e1d75a47f4d49a5d679d83c7469cd
Matthew Kogoy <matthew.kogoy@gmail.com>
2023-07-01 12:07:51 +0200

    Have renderCharacter function render the character

--- index.js
+++ index.js
@@ -5,3 +5,87 @@ Challenge #1:
    JavaScript.
 2. Do the same for Orc card. 
 */
+
+
+// const heroDiv = document.getElementById('hero')
+
+// heroDiv.innerHTML = 
+// `
+//    <div class="character-card">
+//          <h4 class="name"> Wizard </h4>
+//          <img class="avatar" src="img/wizard.png"/>
+//          <p class="health">health: <b> 60 </b></p>
+//          <div class="dice-container"><div class="dice"> 6 </div></div>
+//    </div>  
+// `
+
+/* But you can write all of that code on one line,
+like this: */
+
+// document.getElementById('hero').innerHTML = `
+//    <div class="character-card">
+//          <h4 class="name"> Wizard </h4>
+//          <img class="avatar" src="img/wizard.png"/>
+//          <p class="health">health: <b> 60 </b></p>
+//          <div class="dice-container">
+//             <div class="dice"> 6 </div>
+//          </div>
+//    </div>  
+// `
+
+// document.getElementById('monster').innerHTML = `
+//    <div class="character-card">
+//       <h4 class="name"> Orc </h4>
+//       <img class="avatar" src="img/orc.png"/>
+//       <p class="health">health: <b> 10 </b></p>
+//       <div class="dice-container">
+//          <div class="dice"> 4 </div>
+//       </div>
+//    </div>
+// `
+
+
+/* Writing Cleaner Code */
+
+
+const hero = {
+   elementId: "hero",
+   name: "Wizard",
+   avatar: "img/wizard.png",
+   health: 60,
+   diceRoll: 6
+}
+
+const monster = {
+   elementId: "monster",
+   name: "Orc",
+   avatar: "img/orc.png",
+   health: 10,
+   diceRoll: 4
+}
+
+
+/* Challenge 3: The renderCharacter function */
+
+
+function renderCharacter(data) {
+/*
+Challenge:
+    1. Make renderCharacter render out the character 
+       from the object it is passed.
+*/
+
+ document.getElementById(data.elementId).innerHTML = `
+   <div class="character-card">
+      <h4 class="name"> ${data.name} </h4>
+      <img class="avatar" src="${data.avatar}"/>
+      <p class="health">health: <b> ${data.health} </b></p>
+      <div class="dice-container">
+         <div class="dice"> ${data.diceRoll} </div>
+      </div>
+   </div> 
+`
+}
+
+renderCharacter(hero)
+renderCharacter(monster)
