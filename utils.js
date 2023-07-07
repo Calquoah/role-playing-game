9347c409eff6989d68c1987ce6afaecc57aed8ad
Matthew Kogoy <matthew.kogoy@gmail.com>
2023-07-07 10:56:57 +0200

    Move getDiceRollArray function into a new file Create a new file to house this function

---
+++ utils.js
@@ -0,0 +1,13 @@
+function getDiceRollArray(diceCount) {  
+    return new Array(diceCount).fill(0).map(function(){
+    return Math.floor(Math.random() * 6) + 1
+    });   
+}
+
+export {getDiceRollArray}
+
+
+
+/* We want a "named" export because
+we will be exporting multiple pieces
+of code from this utils file */
