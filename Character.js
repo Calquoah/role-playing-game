5b8368721fbdc1220c238631415529e57c3111d2
Matthew Kogoy <matthew.kogoy@gmail.com>
2023-07-27 18:13:53 +0200

    Use arrow functions to refactor dice array

--- Character.js
+++ Character.js
@@ -459,7 +459,92 @@
 
 
 
-/* Challenge: Is the Character Dead */
+/* Challenge: Is the Character Dead? */
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { getDiceRollArray, getDicePlaceholderHtml } from './utils.js'
+
+// function Character(data) {
+//     Object.assign(this, data)
+
+//     this.diceArray = getDicePlaceholderHtml(this.diceCount)
+
+//     this.getDiceHtml = function () {
+//         this.currentDiceScore = getDiceRollArray(this.diceCount);
+//         this.diceArray = this.currentDiceScore.map(function(num){
+//             return `<div class="dice">${num}</div>`
+//             }).join("")
+//     }
+    
+// /*
+// CHALLENGE
+// 1. Add a line of code inside the body of the if statement
+// in the takeDamage method which will give the character a 
+// new boolean "dead" when health reaches zero. It can be initialised with "true".
+// 2. For now, log out that boolean when the character's 
+// health reaches zero.
+// */
+    
+//     this.takeDamage = function(attackScoreArray){  
+//         const totalAttackScore = attackScoreArray.reduce(function(total, num) {
+//             return total + num
+//             })
+//         this.health -= totalAttackScore
+//         if (this.health <= 0){
+//             this.dead = true 
+//             this.health = 0
+//         } 
+//         console.log(this.dead)
+//     }
+ 
+//     this.getCharacterHtml = function () {
+//         const { elementId, name, avatar, health, diceCount } = this;
+
+//         return `
+//             <div class="character-card">
+//                 <h4 class="name"> ${name} </h4>
+//                 <img class="avatar" src="${avatar}" />
+//                 <div class="health">health: <b> ${health} </b></div>
+//                 <div class="dice-container">
+//                     ${this.diceArray}
+//                 </div>
+//             </div>`
+//     }
+// }
+
+// export default Character
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Challenge: Using Arrow Functions */
+
+
 
 
 
@@ -479,35 +564,60 @@ function Character(data) {
     Object.assign(this, data)
 
     this.diceArray = getDicePlaceholderHtml(this.diceCount)
-
-    this.getDiceHtml = function () {
-        this.currentDiceScore = getDiceRollArray(this.diceCount);
-        this.diceArray = this.currentDiceScore.map(function(num){
-            return `<div class="dice">${num}</div>`
-            }).join("")
-    }
     
 /*
 CHALLENGE
-1. Add a line of code inside the body of the if statement
-in the takeDamage method which will give the character a 
-new boolean "dead" when health reaches zero. It can be initialised with "true".
-2. For now, log out that boolean when the character's 
-health reaches zero.
+1. In the getDiceHtml method, there is an anonymous function 
+being used as an inline callback function. Replace it with an 
+arrow function.
+2. Do the same for the anonymous function in the takeDamage 
+method.
+3. Try to end up with the least amount of code possible!
 */
+
+    /* My Solution */
+
+    this.getDiceHtml = function () {
+        this.currentDiceScore = getDiceRollArray(this.diceCount);
+        this.diceArray = this.currentDiceScore.map(num => 
+            `<div class="dice">${num}</div>`).join("")
+    }
+
+    /* Tom's Solution */
+
+    /*   this.getDiceHtml = function () {
+        this.currentDiceScore = getDiceRollArray(this.diceCount);
+        this.diceArray = this.currentDiceScore.map((num)=> 
+            `<div class="dice">${num}</div>`).join("")
+    } */
+
+
+    /* My Solution */
     
     this.takeDamage = function(attackScoreArray){  
-        const totalAttackScore = attackScoreArray.reduce(function(total, num) {
-            return total + num
-            })
+        const totalAttackScore = attackScoreArray.reduce((total, num) => total + num)
         this.health -= totalAttackScore
         if (this.health <= 0){
-            this.dead = true 
+            this.dead = true  
             this.health = 0
         } 
-        console.log(this.dead)
     }
- 
+
+
+    /* Tom's Solution */
+
+
+    /*   this.takeDamage = function(attackScoreArray){  
+        const totalAttackScore = attackScoreArray.reduce((total, num) => total + num)
+        this.health -= totalAttackScore
+        if (this.health <= 0){
+            this.dead = true  
+            this.health = 0
+        } 
+    } */
+
+
+    
     this.getCharacterHtml = function () {
         const { elementId, name, avatar, health, diceCount } = this;
 
